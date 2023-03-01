cb30099781838b97db457d96607c6578d944b0f3
CristianoAlberto <cristianoalberto908347@gmail.com>
2023-03-01 18:26:28 +0100

    Importando Componente

--- fundamentos/src/components/FirstComponents.jsx
+++ fundamentos/src/components/FirstComponents.jsx
@@ -1,10 +1,10 @@
 const FirstComponent = () =>{
     return (
         <div>
-            <h2>Meu primeiro componente</h2>
+            <h2>Meu primeiro componente!</h2>
         </div>
     )
 
 }
 
-exports default FirstComponent
+export default FirstComponent
