5fa0dfce0c28ac89da9f90bb658cf9f54251c62c
CristianoAlberto <cristianoalberto908347@gmail.com>
2023-03-01 20:10:59 +0100

    Funçaõ de renderização

--- fundamentos/src/components/Events.jsx
+++ fundamentos/src/components/Events.jsx
@@ -6,6 +6,16 @@ function Events() {
     console.log("Testanto evento");
     console.log(e);
   }
+
+  const renderSomething = (x) => {
+    if(x){
+      return <h1>Renderizando isso!</h1>
+    }else{
+      return <h1>Rederizando outra coisa!</h1>
+    }
+  }
+
+  
   return (
     <div>
       <div>
@@ -19,6 +29,8 @@ function Events() {
           Clique aqui 2
         </button>
       </div>
+      {renderSomething(true)}
+      {renderSomething(false)}
     </div>
   )
 }
