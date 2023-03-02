ca0ccd64ab088d60c69499b9994b15edba9864f0
CristianoAlberto <cristianoalberto908347@gmail.com>
2023-03-02 23:59:03 +0100

    Adicionando imagem

--- avancando_em_react/src/App.jsx
+++ avancando_em_react/src/App.jsx
@@ -7,26 +7,8 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Avançando em react</h1>
+      <img src="imagem1.jpg" alt='Alguma imagem'/>
     </div>
   )
 }
