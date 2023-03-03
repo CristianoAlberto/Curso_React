8ca6ad1edd72f746eb2fa0612f357041c5947dab
CristianoAlberto <cristianoalberto908347@gmail.com>
2023-03-03 01:49:13 +0100

    Adcionando um else

--- avancando_em_react/src/components/CondicionalRender.jsx
+++ avancando_em_react/src/components/CondicionalRender.jsx
@@ -2,14 +2,23 @@ import React from 'react'
 
 const CondicionalRender = () => {
 
-    const x = false;
-
+    const x = true;
+    const name = 'Chris';
 
   return (
     <div>
         <h3>Isso sera exibido?</h3>
-        {x && <p>Se x for true sim!</p>}
+        {x ? <p>Se x for true sim!</p>:<p>Se for false não!</p>}
 
+        {name === "Chris" ? (
+            <div>
+                <p>Olá Chris</p>
+            </div>
+        ): (
+            <div>
+                <p>Nome não encontrado</p>
+            </div>
+        )}
     </div>
   )
 }
