38bb4085696245a32f4c95cae8aeaad1275af6d4
CristianoAlberto <cristianoalberto908347@gmail.com>
2023-03-03 01:19:44 +0100

    Previus state

--- avancando_em_react/src/components/ListeRender.jsx
+++ avancando_em_react/src/components/ListeRender.jsx
@@ -3,12 +3,20 @@ import { useState } from "react"
 const ListeRender = () => {
 
     const [list] = useState(["Chris", "Alberto", "Demolidor"]);
-    
+
     const [users, setUsers] = useState([
-        {id:1, name:"Chris", age: 19},
-        {id:2, name:"Alberto", age: 20},
-        {id:3, name:"Demolidor", age: 25}
+        { id: 1, name: "Chris", age: 19 },
+        { id: 2, name: "Alberto", age: 20 },
+        { id: 3, name: "Demolidor", age: 25 }
     ])
+
+    const deleteRandom = () => {
+        const randomNumber = Math.floor(Math.random() * 5);
+
+
+        setUsers(users.filter(user => user.id !== randomNumber));
+    }
+
     return (
         <div>
             <ul>
@@ -17,10 +25,11 @@ const ListeRender = () => {
                 ))}
             </ul>
             <ul>
-                {users.map((user) =>(
+                {users.map((user) => (
                     <li key={user.id}>{user.name} - {user.age}</li>
                 ))}
             </ul>
+            <button onClick={()=>deleteRandom()}>Deletar user</button>
         </div>
     )
 }
